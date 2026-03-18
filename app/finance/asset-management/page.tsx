22a5447f41b87c1e9062112afa38ae058362c101
rorakshe <rohit.rakshe18@gmail.com>
2026-03-18 11:09:04 +0530

    space added
    space added

--- app/finance/asset-management/page.tsx
+++ app/finance/asset-management/page.tsx
@@ -167,7 +167,7 @@ export default function AssetManagementPage() {
                 </tr>
               ))}
             </tbody>
-          </table>
+          </table> 
         </div>
       )}
 
